94368e324e241202744e6ee6511def39964e2d50
Pavel <88274963+PavelFeborovi@users.noreply.github.com>
2022-10-23 11:13:25 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+
+
+
+function rts(){
+	document.getElementById('exa').style.display='block'
+	document.getElementById('ex1').style.display='block'
+	document.getElementById('btn').style.display='none'
+	document.getElementById('all').style.display='none'
+}
+
+
